ccb2065d1147af56b5c1a929635f63443e469205
Alex Potsides <alex@achingbrain.net>
2023-03-01 15:18:40 +0000

    fix: make pbStream function polymorphic (#40)
    What we want to say is that the type returned from `unwrap` will be
    the original duplex passed in, but it's source will have changed to
    return `Uint8ArrayList`s.  We can still pass duplexes that source
    `Uint8Array`s though, so make the exported function polymorphic.

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -65,7 +65,7 @@ Object.keys(tests).forEach(key => {
   const test = tests[key]
 
   describe(`it-pb-rpc ${key}`, () => {
-    let w: ProtobufStream<Uint8Array>
+    let w: ProtobufStream
 
     before(async () => {
       w = pbStream(pair<Uint8Array>())
